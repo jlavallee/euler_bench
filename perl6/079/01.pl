b9a059773ae3be3ce648921c7343836352e2c995
Jeff Lavallee <jeff@zeroclue.com>
2009-09-24 18:47:28 -0700

    perl6 solution to 079 getting closer, I think

--- perl6/079/01.pl
+++ perl6/079/01.pl
@@ -22,61 +22,43 @@ $passcode.chomp;
 say "passcode: $passcode";
 
 for @entries -> $entry {
-    chomp $entry;
+    chomp $entry;  say "entry: $entry";
 
     my ($first, $second, $third) = $entry.comb;
 
     my $first_chunk  = $entry.substr(0, 2);
     my $second_chunk = $entry.substr(1, 3);
 
-    #say "first: $first";
-    #say "second: $second";
-    #say "third: $third";
-    #say "first chunk: $first_chunk";
-    #say "second chunk: $second_chunk";
+    say "first: $first";
+    say "second: $second";
+    say "third: $third";
+    say "first chunk: $first_chunk";
+    say "second chunk: $second_chunk";
 
-    # insert new digits
-    eval "regex fu \{ $first \}";
-    $passcode = $passcode.subst( rx/<fu>/, $first_chunk )  unless $passcode ~~ $second;
-    eval "regex gu \{ $second \}";
-    $passcode = $passcode.subst( rx/<gu>/, $first_chunk )  unless $passcode ~~ $first;
-    $passcode = $passcode.subst( rx/<gu>/, $second_chunk ) unless $passcode ~~ $third;
-    eval "regex hu \{ $third \}";
-    $passcode = $passcode.subst( rx/<hu>/, $second_chunk ) unless $passcode ~~ $second;
-
-    #$passcode = $passcode.comb.uniq.join('');
-
-    my $pre_swap = $passcode;
 
     eval "regex first_re  \{ $first  \}";
     eval "regex second_re \{ $second \}";
     eval "regex third_re  \{ $third  \}";
 
-    $passcode = $passcode.subst( rx/<second_re><first_re>/, { $first ~  $second } );
-    $passcode = $passcode.subst( rx/<third_re><second_re>/, { $second ~  $third } );
+    # insert new digits
+    $passcode = $passcode.subst( rx/<first_re>/,  { $first_chunk } ) unless $passcode ~~ $second;
+    $passcode = $passcode.subst( rx/<second_re>/, { $first_chunk } ) unless $passcode ~~ $first;
+    $passcode = $passcode.subst( rx/<second_re>/, { $second_chunk} ) unless $passcode ~~ $third;
+    $passcode = $passcode.subst( rx/<third_re>/,  { $second_chunk} ) unless $passcode ~~ $second;
+
+    #$passcode = $passcode.comb.uniq.join('');
+    $passcode = $passcode.comb.uniq.join('');
+
+    #my $pre_swap = $passcode;
+
+    #$passcode = $passcode.subst( rx/<second_re><first_re>/, { $first ~  $second } );
+    #$passcode = $passcode.subst( rx/<third_re><second_re>/, { $second ~  $third } );
     
-    #eval "regex swap_second_and_first \{ $second(.*)$third \}";
+    #eval "regex swap_second_and_first \{ $second(.*)$first \}";
     #eval "regex swap_third_and_second \{ $third(.*)$second \}";
 
-    #$passcode = $passcode.subst( rx/<swap_second_and_first>/, { $first ~ $0 ~ $second } );
-    #$passcode = $passcode.subst( rx/<swap_third_and_second>/, { $second ~ $0 ~ $third } );
-
-    #if ! $passcode =~ /$second/ {
-    #    if $passcode =~ /$first/ {
-    #        my $index = $passcode.index($first);
-    #        $passcode.subst($index, $index+1) = $first_chunk;
-    #    }
-    #    #$passcode.substr(0, 1) = $first_chunk;
-    #}
-    ##unless $passcode =~ /$first/ {
-    #   $passcode.substr(1, 1) = $first_chunk;
-    ##}
-    ##unless $passcode =~ /$third/ {
-    #   $passcode.substr(1, 1) = $second_chunk;
-    ##}
-    ##unless $passcode =~ /$second/ {
-    #    $passcode.substr(2, 1) = $second_chunk;
-    ##}
+    $passcode = $passcode.subst( rx/<second_re>(.*)<first_re>/, { $first ~ $0 ~ $second } );
+    $passcode = $passcode.subst( rx/<third_re>(.*)<second_re>/, { $second ~ $0 ~ $third } );
 
     #$passcode =~ s/$second(.*)$first/$first$1$second/;
     #$passcode =~ s/$third(.*)$second/$second$1$third/;
